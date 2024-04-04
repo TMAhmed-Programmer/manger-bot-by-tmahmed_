ab3e5ee9024ba031e220c6b8e2c20ba3e1f7914d
Hira® Studio <155318077+hirastudio@users.noreply.github.com>
2024-04-04 16:33:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -672,7 +672,192 @@ client.on('messageCreate', async message => {
 });
 
 //sugg pre
-////////////////////////////////////////////
+
+//تعامل مع  رسائل
+const channelIds = ['1223738263039250533', '1222553840927379587'];
+
+client.on('messageCreate', async message => {
+  if (channelIds.includes(message.channelId) && message.content.toLowerCase() === 'off') {
+    const embed = new MessageEmbed()
+      .setTitle('<a:ArabTeam_No:1224096601514049598> **The Bot Is Offline**')
+      .setDescription('تم ايقاف البوت موقتا لاضافة تحديثات.')
+      .setColor('#FF0000');
+
+    const sentMessage = await message.channel.send({ embeds: [embed] });
+  }
+});
+
+
+client.on('messageCreate', async message => {
+  if (channelIds.includes(message.channelId) && message.content.toLowerCase() === 'on') {
+    const embed = new MessageEmbed()
+      .setTitle('<a:ArabTeam_Yes:1224096599903436952> **The Bot Is Online**')
+      .setDescription('البوت جاهز للعمل.')
+      .setColor('#2aff00');
+
+    const sentMessage = await message.channel.send({ embeds: [embed] });
+  }
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const characters = [
+    { name: 'ايانوكوجي', image: 'https://media.discordapp.net/attachments/1183299827996557363/1224261615248085013/Classroom_Of_The_Elite_-_Icon_.jpg?ex=661cd97a&is=660a647a&hm=956b75028bad96e6dc9279167017ae88cdfb88b93db2efb2358840118390f7b0&=&format=webp&width=608&height=608' },
+    { name: 'غوجو ساتورو', image: 'https://media.discordapp.net/attachments/1183299827996557363/1224261613339545650/293c9db14a39ea99.jpg?ex=661cd97a&is=660a647a&hm=4236e1f9f0b6ef9b9d2b4d844dfb5d0fdc6511e992fb34d799a8a3bcb5dfda28&=&format=webp' },
+    { name: 'كورابيكا', image: 'https://media.discordapp.net/attachments/1183299827996557363/1224261614400835584/bdcde00832c2d963.jpg?ex=661cd97a&is=660a647a&hm=fef1778d47e32013f41a42c0dcf2a2fb43f6c13cf3338b341caaa8f15f5507a9&=&format=webp' },
+    { name: 'سون غوكو', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224723098247630980/1712067018684.jpg?ex=661e8744&is=660c1244&hm=c2d9b72064104ba513c4de282f7e4df1b3d95be2ac907c3ca6243544a3ce2cdc&=&format=webp' },
+    { name: 'فريزا', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224723189574139994/1712067062732.jpg?ex=661e875a&is=660c125a&hm=cc0eb2ecd2e17d05ebb9da2026b3ff02399fb476921af7b1e3eb5681ede90146&=&format=webp' },
+    { name: 'فيجيتا', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224723257274531931/1712067071943.jpg?ex=661e876a&is=660c126a&hm=358a239ccd8c89020d3a87a51fef0ac58230102b483eb485d63e804399e9757f&=&format=webp' },
+    { name: 'اغريس', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224723545096060948/1712067204965.jpg?ex=661e87af&is=660c12af&hm=294b69540d640a29293b4cfe490fd9df35682dbe2d3761284ab41bad1131d422&=&format=webp&width=504&height=608' }, 
+    { name: 'سونغ جين', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224723753087402004/1712067258613.png?ex=661e87e1&is=660c12e1&hm=eb94e1c2e8e4bb80ce8c44d159df0f0f765d280d188791a058ba85bb1fdba35b&=&format=webp&quality=lossless&width=391&height=608' },
+    { name: 'لوفي', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224723887972024485/1712067311689.jpg?ex=661e8801&is=660c1301&hm=f6ffabdc98cd5834019dcd561ff4198a23a09e48aa9d2b3ca84c62ce3a106353&=&format=webp' },
+    { name: 'زورو', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224724275206819870/1712067343104.jpg?ex=661e885d&is=660c135d&hm=f8855c17e329db7b008dafd59e628f7673aeaba4d64cfbc8d9b7a7fd493a0d39&=&format=webp&width=350&height=350' },
+    { name: 'شانكس', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224724339941703781/1712067359101.jpg?ex=661e886c&is=660c136c&hm=2faa8526eb8414a0f2a067561ec4eb6ccdd423df68b6d7bda1df17f26337134b&=&format=webp' },
+    { name: 'الحية البيضاء', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224724405318451231/1712067353415.jpg?ex=661e887c&is=660c137c&hm=6b89c9168414e9dbe098a8cbe8285b87e97e06ae301e48ce32e2f39092c21178&=&format=webp' },
+    { name: 'روجر', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224724466987307170/1712067364902.jpg?ex=661e888b&is=660c138b&hm=d03bcfa5f78532f489fb95797df5e0bed65a45272d5abbceadc44e75e9e89dba&=&format=webp' },
+    { name: 'لاو', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224724740111863818/1712067372239.jpg?ex=661e88cc&is=660c13cc&hm=a7a1247ca7658e6a6115066fc602b28b897a43ad8d0fd633e19f02ee4245a59f&=&format=webp&width=608&height=608' },
+    { name: 'كيلوا', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224818973367865384/Killua.jpg?ex=661ee08f&is=660c6b8f&hm=e23f651f36b7bd474df6a2e2e5acca1b7917573a691fe6b9c3a213a86ae8b356&=&format=webp&width=608&height=608' },
+    { name: 'سوكونا', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224819025377235045/1ec10cc0-271f-4ad0-a9ca-a53bc4b0537d.jpg?ex=661ee09b&is=660c6b9b&hm=dba91d2a5deb67e431e069028b76795ad88e0dea2fd64858c9d556c8615fe0d8&=&format=webp&width=351&height=350' },
+    { name: 'ايرين', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224818973128917002/-_.jpg?ex=661ee08f&is=660c6b8f&hm=d130971f48e15a2ee101596fffad6fc1ce73334f37934a05878158326916f0df&=&format=webp&width=608&height=608' },
+       { name: 'داكي', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224873744493969428/1712103038297.jpg?ex=661f1391&is=660c9e91&hm=62d0e489322dc620aafe44f92bc69d8912ebbb141d056d39b140202f3344eb8d&=&format=webp' },
+     { name: 'أوباياشيكي كاجايا', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224873390670872668/1712102661049.jpg?ex=661f133d&is=660c9e3d&hm=618512d700d94cc32be56da166f6459313e41e246148d352d1d12c9d986082c2&=&format=webp' }, 
+     { name: 'كيرا', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224873063603245226/1712102440531.jpg?ex=661f12ef&is=660c9def&hm=f987658881fe17ea791f6a183f27d0e16a76b6cb80eebf20819fa954c3e3b3e6&=&format=webp&width=391&height=608' },
+     { name: 'كانجي', image: 'https://media.discordapp.net/attachments/1224406657287852186/1224873000172523561/1712102564191.jpg?ex=661f12e0&is=660c9de0&hm=eeeb14ec59a08a88a260dae60d9aa7d9a30e71500f34d4806be4b376bab06bc7&=&format=webp' },
+     { name: 'ناجي', image: 'https://media.discordapp.net/attachments/1224406657287852186/1225018273670758430/Nagi.jpg?ex=661f9a2c&is=660d252c&hm=9575db835c57b4aa3f91ebede4f12c67c078c08e49720f6561b2e6a86c5dfa67&=&format=webp&width=608&height=608' },
+     { name: 'رايزل', image: 'https://media.discordapp.net/attachments/1224406657287852186/1225018546099195934/4e950c27e3d4e5f8.jpg?ex=661f9a6d&is=660d256d&hm=5b581d234166873e8eef150a4d5d9175b70ac2bfe4e5070263eb88e3fba32cc7&=&format=webp&width=608&height=608' },
+     { name: 'فيوليت', image: 'https://media.discordapp.net/attachments/1224406657287852186/1225018852740567110/1.jpg?ex=661f9ab6&is=660d25b6&hm=247f46858fce7dd7ae5630ca7e1e230f03c4fc13db0f51f8b771e4690ff3a25a&=&format=webp&width=664&height=608' },
+    // Add more characters with their images here
+];
+
+
+let lastCharacter = null; // Variable to store the last character used
+
+client.on('messageCreate', async message => {
+    if (message.content === '#anime-guess') {
+        let character = characters[Math.floor(Math.random() * characters.length)];
+
+        // Ensure the current character is not the same as the last character used
+        while (character === lastCharacter) {
+            character = characters[Math.floor(Math.random() * characters.length)];
+        }
+
+        // Update the last character variable with the current character
+        lastCharacter = character;
+
+        const embed = new MessageEmbed()
+            .setTitle('Guess the Character')
+            .setDescription(`Guess the name of this character within 10 seconds.`)
+            .setImage(character.image)
+            .setColor('#0099ff');
+
+        const sentMessage = await message.channel.send({ embeds: [embed] });
+
+        const filter = m => m.author.id === message.author.id;
+        const collector = message.channel.createMessageCollector({ filter, time: 10000 });
+
+        collector.on('collect', m => {
+            collector.stop(); // Stop the collector immediately after collecting a message
+            if (m.content.toLowerCase() === character.name.toLowerCase()) {
+                sentMessage.edit(`Congratulations ${m.author}! You guessed the character correctly. It's ${character.name}.`);
+
+                const winnerEmbed = new MessageEmbed()
+                    .setTitle('Congratulations!')
+                    .setDescription(`Congratulations ${m.author}! You guessed the character correctly.`)
+                    .addField('Winner', m.author.username)
+                    .addField('Correct Answer', character.name)
+                    .setColor('#00ff00');
+                message.channel.send({ embeds: [winnerEmbed] });
+            } else {
+                sentMessage.edit(`Hard luck! You didn't guess the character correctly. The correct answer is ${character.name}.`);
+            }
+        });
+
+        collector.on('end', () => {
+            if (!sentMessage.deleted) { // Check if the message is not deleted
+                if (!sentMessage.embeds[0].description.includes('You guessed')) {
+                    sentMessage.edit(`Time's up! You didn't guess the character. The correct answer is ${character.name}.`);
+                }
+            }
+        });
+    }
+});
+
+//fakk
+const words = [
+    'apple', 'banana', 'orange', 'grape', 'kiwi', 'hira', 'sad', 'happy', 'forget', 'games',
+    'anime', 'egypt', 'iraq', 'arabic', 'english', 'youtube', 'بؤبؤ', 'حواذث', 'kaniki', 'law',
+    'GiRle', 'bOy', 'sukona', 'baka', 'akira', 'henokamy', 'kagora', 'kaigaku', 'كايجاكو',
+    'هينوكامي', 'كاغورا', 'image', 'cat', 'dog', 'house', 'computer', 'phone', 'table', 'chair',
+    'book', 'paper', 'pencil', 'flower', 'car', 'tree', 'mountain', 'beach', 'music', 'movie',
+    'friend', 'family', 'school', 'teacher', 'student', 'glass', 'water', 'fire', 'earth', 'air',
+    'sun', 'moon', 'star', 'planet', 'universe', 'galaxy', 'lion', 'tiger', 'elephant', 'zebra',
+    'giraffe', 'rhinoceros', 'hippopotamus', 'crocodile', 'snake', 'eagle', 'hawk', 'owl', 'sparrow',
+    'penguin', 'koala', 'kangaroo', 'camel', 'rabbit', 'fox', 'wolf', 'bear', 'deer', 'squirrel',
+    'whale', 'dolphin', 'shark', 'octopus', 'jellyfish', 'seahorse', 'turtle', 'frog', 'toad', 'lizard',
+    'flowerpot', 'guitar', 'violin', 'drum', 'keyboard', 'microphone', 'camera', 'television', 'radio',
+    'newspaper', 'magazine', 'poster', 'painting', 'sculpture', 'statue', 'bridge', 'building', 'road',
+    'traffic', 'airport', 'train', 'bus', 'subway', 'ship', 'boat', 'helicopter', 'airplane', 'rocket'];
+
+let lastWord = ''; // تخزين آخر كلمة تم استخدامها
+
+// ترتيب الحروف بشكل عشوائي
+function scrambleWord(word) {
+    const characters = word.split('');
+    const scrambledCharacters = [];
+
+    // توليد ترتيب عشوائي للحروف
+    while (characters.length > 0) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        const randomCharacter = characters[randomIndex];
+        scrambledCharacters.push(randomCharacter);
+        characters.splice(randomIndex, 1);
+    }
+    
+    return scrambledCharacters.join('');
+}
+
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    if (message.content.toLowerCase() === '#word-guess') {
+        let word = getRandomWord();
+        
+        // التأكد من أن الكلمة الجديدة مختلفة عن الكلمة السابقة
+        while (word === lastWord) {
+            word = getRandomWord();
+        }
+        
+        const scrambledWord = scrambleWord(word);
+        message.reply(`Guess the word of this letters within 10 seconds : **${scrambledWord}**`);
+        
+        const filter = response => response.author.id === message.author.id && typeof response.content === 'string' && response.content.trim() !== '';
+        const collector = message.channel.createMessageCollector({ filter, max: 1, time: 15000 }); // جمع الإجابات لمدة 15 ثانية
+        
+        collector.on('collect', response => {
+            if (response.content.toLowerCase().trim() === word) {
+                message.reply(`Congratulations ${message.author.username}! You guessed the word correctly.`);
+            } else {
+                message.reply(`Hard luck ${message.author.username}! You didn't guess the word correctly. The correct answer is **${word}**.`);
+            }
+        });
+        
+        collector.on('end', collected => {
+            if (collected.size === 0) {
+                message.reply(`Time's up ${message.author.username}! You didn't guess the word. The correct answer is **${word}**.`);
+            }
+        });
+        
+        lastWord = word; // تحديث آخر كلمة تم استخدامها
+    }
+});
+
+// توليد كلمة عشوائية من القائمة
+function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+}
+
 
 //by hira studio
 //by hira studio
